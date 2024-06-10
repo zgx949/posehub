5a274d0e5b29b1f2cd68d1a124534f5708a309d3
guoxiang <1637349696@qq.com>
2024-06-10 16:12:20 +0800

    🎉feat: added pose api

--- src/api/posehub.js
+++ src/api/posehub.js
@@ -1,3 +1,10 @@
 import axios from 'axios'
+import request from "../utils/request.js";
 export const getPoseList = () =>  axios.get('/hot.json')
-export const getPoseCategory = () =>  axios.get('/category.json')
+export const getPoseCategory = async () => {
+    const res = await request.get('/category/common?category_id=45&openid=ofJJv6A9EdBnYftPSNqNhQJ9Wrds')
+    debugger
+    return {
+        data: res.data.category_list["0"]
+    }
+}
---
+++ src/utils/request.js
@@ -0,0 +1,47 @@
+import axios from 'axios'
+const baseURL = 'https://diary.froglesson.com'
+const request = axios.request
+
+import axios from 'axios';
+
+// 设置基础URL
+const BASE_URL = 'https://your-base-api-url.com/';
+
+// 创建axios实例
+const request = axios.create({
+    baseURL: BASE_URL, // 设置基础URL
+    timeout: 5000, // 设置请求超时长，单位ms
+    headers: { // 默认请求头信息
+        'Content-Type': 'application/json',
+        // 其他头信息
+    },
+});
+
+// 请求拦截器
+request.interceptors.request.use(config => {
+    // 在发送请求前的操作
+    // 例如，添加Token
+    if (localStorage.getItem('token')) {
+        config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
+    }
+
+    return config
+}, error => Promise.reject(error));
+
+// 响应拦截器
+axiosInstance.interceptors.response.use(response => {
+    // 成功处理响应
+    return response.data; // 直接返回data部分
+}, error => {
+    // 错误处理
+    if (error.response) {
+        console.error(`Error: ${error.response.status} - ${error.response.statusText}`);
+    } else if (error.request) {
+        console.error('No response received.');
+    } else {
+        console.error('Error setting up the request', error.message);
+    }
+    return Promise.reject(error);
+});
+
+export default request;
